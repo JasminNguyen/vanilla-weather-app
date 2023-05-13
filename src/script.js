90add68bd5687e873038aa06682ef6e3d914ed02
JasminNguyen <jasminnguyen001@web.de>
2023-05-13 16:05:04 +0200

    Showing the current temperature

--- src/script.js
+++ src/script.js
@@ -1,1 +1,26 @@
+function displayTemperature(response) {
+  console.log(response.data);
+  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#big-temperature");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
 
+  let cityElement = document.querySelector("#requested-city");
+  cityElement.innerHTML = response.data.name;
+
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let apiKey = "0c1ee4d3bfdf1c40c029b600a2b66dd3";
+let city = "London";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
